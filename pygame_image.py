c8c75d17b95697512300abf0e3458791d5773129
KATO Yuki <c0b23039a5@apps.teu.ac.jp>
2024-11-19 16:08:11 +0900

    練習８：こうかとんが移動するように

--- pygame_image.py
+++ pygame_image.py
@@ -14,13 +14,25 @@ def main():
     koukaton_img = pg.transform.flip(koukaton_img,True,False)
     bg_img_flip = pg.transform.flip(bg_img,True,False)
     tmr = 0
+    kk_rct= koukaton_img.get_rect()
+    kk_rct.center = 300,200
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-
-        screen.blit(bg_img, [-(tmr%3200), 0]) # screen Surfaceに背景画像を貼り付ける
+        key_lst = pg.key.get_pressed()
+        screen.blit(bg_img, [-(tmr%3200), 0])
         screen.blit(bg_img_flip, [-(tmr%3200)+1600, 0])
-        screen.blit(koukaton_img,[300,200])
+        screen.blit(bg_img, [-(tmr%3200)+3200, 0])
+        screen.blit(bg_img_flip, [-(tmr%3200)+4800, 0])
+        if key_lst[pg.K_UP]:
+            kk_rct.move_ip((0,-1))
+        if key_lst[pg.K_DOWN]:
+            kk_rct.move_ip((0,1))
+        if key_lst[pg.K_LEFT]:
+            kk_rct.move_ip((-1,0))
+        if key_lst[pg.K_RIGHT]:
+            kk_rct.move_ip((1,0))
+        screen.blit(koukaton_img,kk_rct.center)
         pg.display.update()
         tmr += 1
         clock.tick(200)
